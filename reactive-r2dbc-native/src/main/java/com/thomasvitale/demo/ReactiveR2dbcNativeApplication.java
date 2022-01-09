29bbc806e10aa1be479a7508ede96eba53d30c59
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-01-10 00:55:01 +0100

    Update config

--- reactive-r2dbc-native/src/main/java/com/thomasvitale/demo/ReactiveR2dbcNativeApplication.java
+++ reactive-r2dbc-native/src/main/java/com/thomasvitale/demo/ReactiveR2dbcNativeApplication.java
@@ -1,5 +1,7 @@
 package com.thomasvitale.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.annotation.Id;
@@ -27,6 +29,7 @@ interface BookRepository extends ReactiveCrudRepository<Book,Long> {}
 @RestController
 class BookController {
 
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
     private final BookRepository bookRepository;
 
     BookController(BookRepository bookRepository) {
@@ -35,11 +38,13 @@ class BookController {
 
     @GetMapping
     Flux<Book> getBooks() {
+        log.info("Fetching list of books");
         return bookRepository.findAll();
     }
 
     @PostMapping
     Mono<Book> addBook(@RequestBody Book book) {
+        log.info("Adding new book to the catalog: {}", book.title());
         return bookRepository.save(book);
     }
 
--- servlet-jdbc-jvm/src/main/java/com/thomasvitale/demo/ServletJdbcJvmApplication.java
+++ servlet-jdbc-jvm/src/main/java/com/thomasvitale/demo/ServletJdbcJvmApplication.java
@@ -1,5 +1,7 @@
 package com.thomasvitale.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.annotation.Id;
@@ -25,6 +27,7 @@ interface BookRepository extends CrudRepository<Book,Long> {}
 @RestController
 class BookController {
 
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
     private final BookRepository bookRepository;
 
     BookController(BookRepository bookRepository) {
@@ -33,11 +36,13 @@ class BookController {
 
     @GetMapping
     Iterable<Book> getBooks() {
+        log.info("Fetching list of books");
         return bookRepository.findAll();
     }
 
     @PostMapping
     Book addBook(@RequestBody Book book) {
+        log.info("Adding new book to the catalog: {}", book.title());
         return bookRepository.save(book);
     }
 
