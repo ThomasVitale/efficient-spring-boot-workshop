a3bf2c80d0910b2ade0f604b333a0b5a91dfd587
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-01-10 23:51:38 +0100

    Update reactive tests

--- reactive-r2dbc-jvm/src/main/java/com/thomasvitale/demo/ReactiveR2dbcJvmApplication.java
+++ reactive-r2dbc-jvm/src/main/java/com/thomasvitale/demo/ReactiveR2dbcJvmApplication.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
